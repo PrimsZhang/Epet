68b54e06810faeb95e4d119f98d7b6cb89b4239b
primszhang <1947866853@qq.com>
2019-07-13 12:00:10 +0800

    这是第三次提交

--- epet/js/detail.js
+++ epet/js/detail.js
@@ -1,27 +1,9 @@
-class TabBox{
-    constructor(box){
 
-        this.menu = box.children("div").children("ul").children("li");
-        this.list = box.children("div").children("div");
-        this.event();
-    }
-    event(){
-        var that = this;
-        this.menu.click(function(){
-             $(this).css({
-            "background":"#ccc"
-        }).siblings().css({
-            "background":"#fff"
-        });
-        that.list.eq($(this).index()).show().siblings().hide();
-        $(this).addClass("change").siblings().removeClass("change");
-        });
 
-    }
-}
+
+
 onload = function(){
-    new TabBox($(".tab"));
-    new TabBox($("#product .picBox"))
+    new Magnifie($("#product .picBox .photoB .pic"))
 }
 
 
